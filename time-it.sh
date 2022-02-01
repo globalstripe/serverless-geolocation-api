7bfcedae0df9457b5e63cf0965f4f00822064e61
Chris Clark <chris.clark@globalstripe.com>
2022-02-01 01:02:29 +0000

    Added time-it.sh

--- time-it.sh
+++ time-it.sh
@@ -1,8 +1,8 @@
 rm response.json
-
 geourl="https://fnha5j12t9.execute-api.eu-west-1.amazonaws.com/dev/location"
 echo "API HealthCheck"
 curl -w "@curl-format.txt" -o /dev/null -s "https://d3djph4x7pwvze.cloudfront.net/_healthcheck"
 echo "GeoIP Lookup"
 curl -w "@curl-format.txt" -o ./response.json -s "$geourl"
-cat response.json | jq '.city.country.iso_code'
+geourl="https://zw2bdu77c4.execute-api.eu-west-1.amazonaws.com/dev/location"
+curl -w "@curl-format.txt" -o /dev/null -s "$geourl"
